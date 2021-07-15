3610d105f3d5cab5e8437ac60ff35874f5b467ca
emagioli <enzomagioli@gmail.com>
2021-07-15 13:30:48 -0300

    Classe Posicao criada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
         }
     }
 }
